759d0e50284b64341a54fdbdca7b7cd51da126bd
Pratiksha Palkar <64685263+Prati-1234@users.noreply.github.com>
2021-12-20 19:42:33 +0530

    Add files via upload

---
+++ ifelse.rb
@@ -0,0 +1,8 @@
+puts "enter age:"
+age=gets.chomp.to_i
+if age>=18
+puts "you can vote.."
+else
+puts "sorry you cant vote.."
+end
+
---
+++ whileloop1.rb
@@ -0,0 +1,5 @@
+a=5
+while a!=0
+puts "Pratiksha Palkar"
+a=a-1
+end
