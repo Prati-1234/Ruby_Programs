9915082dce6be9863bc360a7321f6d67cca5d218
Prati-1234 <pratikshapalkar141@gmail.com>
2021-12-28 10:45:25 +0530

    hash programs

---
+++ InsertingAndDeleting.rb
@@ -0,0 +1,11 @@
+play_hash={
+    "game"=>"cricker"
+}
+play_hash["players"]=12
+
+puts play_hash
+puts 
+play_hash.delete["players"]
+play_hash=>{"game"="cricket"
+puts play_hash
+
---
+++ hashmethods.rb
@@ -0,0 +1,14 @@
+name={"name"=>"Pratiksha",
+ "Address"=>"Pune",
+ "Age"=>23,
+ "Qualification"=>"BE Comp"
+}
+name2={"name":"Pratiksha",
+    "Address":"Pune",
+    "Age":23,
+    "Qualification":"BE Comp"
+   }
+puts name.size
+puts name.length
+puts name["name"]
+puts name2[:"Qualification"]
