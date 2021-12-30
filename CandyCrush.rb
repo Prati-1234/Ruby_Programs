abc06a9c902d286540031f7ba67fe5b678dbf7d8
Prati-1234 <pratikshapalkar141@gmail.com>
2021-12-30 17:15:06 +0530

    candy crush code updated.

--- CandyCrush.rb
+++ CandyCrush.rb
@@ -4,7 +4,7 @@ def assign_data
 
     $arr.each_with_index { |item, idx| puts "#{item} " }
 
-
+while true
 puts "Enter value from 1-9:"
 value=gets.chomp.to_i
 puts "Enter index row:"
@@ -18,21 +18,25 @@ c=gets.chomp.to_i
         $arr[r][c] = nil
         $arr[r][c-1] = nil
         $arr[r][c-2] = nil
+        show_array
 
     elsif $arr[r][c]== $arr[r][c+1] && $arr[r][c+1]== $arr[r][c+2] && $arr[r][c+2]== $arr[r][c+3] 
         $arr[r][c] = nil
         $arr[r][c+1] = nil
         $arr[r][c+2] = nil
         $arr[r][c+3] = nil
+        show_array
     elsif $arr[r][c]== $arr[r-1][c] && $arr[r-1][c]==$arr[r-2][c] && $arr[r][c]==$arr[r-2][c]
         $arr[r][c] = nil
         $arr[r-1][c]= nil
         $arr[r-2][c] =nil
+        show_array
 
     elsif $arr[r][c]==$arr[r+1][c] && $arr[r][c]==$arr[r-1][c] && $arr[r+1][c]==$arr[r-1][c]
         $arr[r][c] = nil
         $arr[r+1][c]=nil
         $arr[r-1][c] =nil
+        show_array
 
     elsif $arr[r][c] == $arr[r][c] && $arr[r][c] == $arr[r][c+1] && $arr[r][c+1] == $arr[r][c+2] && $arr[r][c+2] == $arr[r][c+3] && $arr[r][c+3] == $arr[r][c+4] && $arr[r][c+4] == $arr[r][c+5]
             $arr[r][c] = nil
@@ -41,11 +45,14 @@ c=gets.chomp.to_i
             $arr[r][c+3] = nil
             $arr[r][c+4] = nil
             $arr[r][c+5] = nil
+            show_array
+
     elsif  $arr[r][c]== $arr[r][c+1] && $arr[r][c]== $arr[r][c-1] && $arr[r][c-1]== $arr[r][c-2]
             $arr[r][c] = nil
             $arr[r][c+1] = nil
             $arr[r][c-1] = nil
             $arr[r][c-2] = nil
+            show_array
 
     elsif $arr[r][c]== $arr[r][c-1] && $arr[r][c-1]== $arr[r][c-2] && $arr[r][c-2]== $arr[r][c-3] && $arr[r][c-3]== $arr[r][c-4]  && $arr[r][c]==$arr[r][c-4]
             $arr[r][c] = nil
@@ -53,21 +60,25 @@ c=gets.chomp.to_i
             $arr[r][c-2] = nil
             $arr[r][c-3] = nil
             $arr[r][c-4] = nil
+            show_array
 
     elsif $arr[r][c]== $arr[r-1][c] && $arr[r][c]==$arr[r+1][c]
            $arr[r][c] = nil
            $arr[r-1][c]= nil
            $arr[r+1][c] =nil
+           show_array
         
     elsif $arr[r][c]==$arr[r][c+1] && $arr[r][c] == $arr[r][c-1]
           $arr[r][c] = nil
           $arr[r][c+1] = nil
           $arr[r][c-1] = nil
+          show_array
 
     elsif $arr[r][c] == $arr[r+1][c] && $arr[r+1][c] == $arr[r+2][c] && $arr[r][c]==$arr[r+2][c]
           $arr[r][c] = nil
           $arr[r+1][c] = nil
           $arr[r+2][c] = nil
+          show_array
           
           
    
@@ -75,15 +86,17 @@ c=gets.chomp.to_i
         $arr[r][c] = nil
         $arr[r-1][c] = nil
         $arr[r-2][c] = nil
+        show_array
 
     elsif $arr[r][c] ==$arr[r-1][c] && $arr[r][c] == $arr[r-2][c]
         $arr[r][c] = nil
         $arr[r-1][c] = nil
         $arr[r-2][c] = nil
+        show_array
 else
     puts "No nil and zero elements in array."
 end
-
+end
 end
 def show_array
     $arr.each_with_index { |item, idx| puts "#{item} " }
