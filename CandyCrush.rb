d816adc567963bb4c5910b304f131775ecabfc8c
Prati-1234 <pratikshapalkar141@gmail.com>
2021-12-30 11:53:27 +0530

    updated code candy crush.

--- CandyCrush.rb
+++ CandyCrush.rb
@@ -1,20 +1,14 @@
 class CandyCrush
-$row=3
-$col=3
+def assign_data
+    $arr=[[1,1,0,nil,3,5],[8,9,2,2,0],[1,0,3,3,0],[1,9,0,2,0],[2,3,4,8,0],[5,8,9,0,6]]
 
-$arr=Array.new($row){Array.new($col)}
-    def assign_data
+for i in 0..5
+    for j in 0..5
+       print  "#{$arr[i][j]}\t"
+end
+puts
+end
 
-        $arr.each_with_index { |item, idx| puts "#{item} " }
-        for i in 0..$row-1
-            for j in 0..$col-1
-        puts "Enter row and columns :"
-       
-        $arr[i][j]=gets.chomp.to_i
-            end
-        end
-        $arr.each_with_index { |item, idx| puts "#{item} " }
-        
 
 puts "Enter value from 1-9:"
 value=gets.chomp.to_i
@@ -24,27 +18,25 @@ puts "Enter index col:"
 c=gets.chomp.to_i
 
         $arr[r][c]=value
-  
-if value==$arr[r][c+1] && value==$arr[r][c+2]  
+    
+    if $arr[r][c] == $arr[r][c-1] && $arr[r][c] == $arr[r][c-2]
+        $arr[r][c] = nil
+        $arr[r][c-1] = nil
+        $arr[r][c-2] = nil
+        
+        if $arr[r][c] == $arr[r][c] && $arr[r+1][c] == $arr[r+2][c]
+            $arr[r][c] = nil
+            $arr[r+1][c] = nil
+            $arr[r+2][c] = nil
+   
 puts "first column deleted..."
 puts
-    $arr[r][c]=nil
-    $arr[r][c+1]=nil
-    $arr[r][c+2]=nil
-elsif  value==$arr[r][c] && value==$arr[r][c+2]
-    $arr[r][c]=nil
-    $arr[r][c+1]=nil
-    $arr[r][c+2]=nil
-
-elsif value==$arr[r][c] && value==$arr[r][c+1]
-    puts 
-    $arr[r][c]=nil
-    $arr[r][c+1]=nil
-    $arr[r][c+2]=nil
+    
 else
     puts "No nil and zero elements in array."
 end
 
+end
 end
 def show_array
     $arr.each_with_index { |item, idx| puts "#{item} " }
