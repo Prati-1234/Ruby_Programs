ec4ff17ea0f2b72cbb1ae54da102d3c278fa18d0
Prati-1234 <pratikshapalkar141@gmail.com>
2021-12-30 16:58:42 +0530

    candy crush code updated.

--- CandyCrush.rb
+++ CandyCrush.rb
@@ -1,6 +1,6 @@
 class CandyCrush
 def assign_data
-    $arr=[[1,1,0,nil,3,5],[8,9,2,2,0],[1,0,3,3,0],[1,9,0,2,0],[2,3,4,8,0],[5,8,9,0,6]]
+    $arr=[[1,1,1,0,3,5],[0,9,2,2,0,6],[1,0,4,3,3,0],[2,8,9,0,9,0],[2,0,0,4,9,6],[0,8,9,6,0,6]]
 
     $arr.each_with_index { |item, idx| puts "#{item} " }
 
@@ -18,24 +18,79 @@ c=gets.chomp.to_i
         $arr[r][c] = nil
         $arr[r][c-1] = nil
         $arr[r][c-2] = nil
-        
-    elsif $arr[r][c] == $arr[r][c] && $arr[r+1][c] == $arr[r+2][c]
-            $arr[r][c] = nil
-            $arr[r+1][c] = nil
-            $arr[r+2][c] = nil
-   
 
-    
+    elsif $arr[r][c]== $arr[r][c+1] && $arr[r][c+1]== $arr[r][c+2] && $arr[r][c+2]== $arr[r][c+3] 
+        $arr[r][c] = nil
+        $arr[r][c+1] = nil
+        $arr[r][c+2] = nil
+        $arr[r][c+3] = nil
+    elsif $arr[r][c]== $arr[r-1][c] && $arr[r-1][c]==$arr[r-2][c] && $arr[r][c]==$arr[r-2][c]
+        $arr[r][c] = nil
+        $arr[r-1][c]= nil
+        $arr[r-2][c] =nil
+
+    elsif $arr[r][c]==$arr[r+1][c] && $arr[r][c]==$arr[r-1][c] && $arr[r+1][c]==$arr[r-1][c]
+        $arr[r][c] = nil
+        $arr[r+1][c]=nil
+        $arr[r-1][c] =nil
+
+    elsif $arr[r][c] == $arr[r][c] && $arr[r][c] == $arr[r][c+1] && $arr[r][c+1] == $arr[r][c+2] && $arr[r][c+2] == $arr[r][c+3] && $arr[r][c+3] == $arr[r][c+4] && $arr[r][c+4] == $arr[r][c+5]
+            $arr[r][c] = nil
+            $arr[r][c+1] = nil
+            $arr[r][c+2] = nil
+            $arr[r][c+3] = nil
+            $arr[r][c+4] = nil
+            $arr[r][c+5] = nil
+    elsif  $arr[r][c]== $arr[r][c+1] && $arr[r][c]== $arr[r][c-1] && $arr[r][c-1]== $arr[r][c-2]
+            $arr[r][c] = nil
+            $arr[r][c+1] = nil
+            $arr[r][c-1] = nil
+            $arr[r][c-2] = nil
+
+    elsif $arr[r][c]== $arr[r][c-1] && $arr[r][c-1]== $arr[r][c-2] && $arr[r][c-2]== $arr[r][c-3] && $arr[r][c-3]== $arr[r][c-4]  && $arr[r][c]==$arr[r][c-4]
+            $arr[r][c] = nil
+            $arr[r][c-1] = nil
+            $arr[r][c-2] = nil
+            $arr[r][c-3] = nil
+            $arr[r][c-4] = nil
+
+    elsif $arr[r][c]== $arr[r-1][c] && $arr[r][c]==$arr[r+1][c]
+           $arr[r][c] = nil
+           $arr[r-1][c]= nil
+           $arr[r+1][c] =nil
+        
+    elsif $arr[r][c]==$arr[r][c+1] && $arr[r][c] == $arr[r][c-1]
+          $arr[r][c] = nil
+          $arr[r][c+1] = nil
+          $arr[r][c-1] = nil
+
+    elsif $arr[r][c] == $arr[r+1][c] && $arr[r+1][c] == $arr[r+2][c] && $arr[r][c]==$arr[r+2][c]
+          $arr[r][c] = nil
+          $arr[r+1][c] = nil
+          $arr[r+2][c] = nil
+          
+          
+   
+    elsif $arr[r][c] ==$arr[r-1][c] && $arr[r-1][c] == $arr[r-2][c] && $arr[r-2][c] == $arr[r-3][c] && $arr[r-3][c] == $arr[r-4][c] && $arr[r-4][c] == $arr[r-5][c]
+        $arr[r][c] = nil
+        $arr[r-1][c] = nil
+        $arr[r-2][c] = nil
+
+    elsif $arr[r][c] ==$arr[r-1][c] && $arr[r][c] == $arr[r-2][c]
+        $arr[r][c] = nil
+        $arr[r-1][c] = nil
+        $arr[r-2][c] = nil
 else
     puts "No nil and zero elements in array."
 end
 
 end
-end
 def show_array
     $arr.each_with_index { |item, idx| puts "#{item} " }
 end
 end
+
+
 candy=CandyCrush.new
 candy.assign_data
 candy.show_array
