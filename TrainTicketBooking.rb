758d178c1f9712580f298647f82952353887c96d
Prati-1234 <pratikshapalkar141@gmail.com>
2022-01-12 10:09:24 +0530

    Train updated code

--- TrainTicketBooking.rb
+++ TrainTicketBooking.rb
@@ -6,10 +6,12 @@ class TrainBooking
           puts "Please enter your choice:"
           puts 
           puts "1.Create Account"
+          puts
           puts "2.Log in"
+          puts
           puts "3.Exit"
-          $choice=gets.chomp.to_i
-          case ($choice)
+          $select_choice=gets.chomp.to_i
+          case ($select_choice)
           when 1
                create_account
           when 2
@@ -23,14 +25,13 @@ class TrainBooking
      def create_account
                puts "Enter Your full username:"
                $username=gets.chomp
+               puts "Enter mobile number:"
+               $number=gets.chomp.to_i
                puts "Enter password:"
                $password=gets.chomp
-               puts "Enter number:"
-               $number=gets.chomp.to_i
                if (($username==nil) || ($username.length<=4)) 
                     puts "Please enter your full username"
                     $username=gets.chomp
-            
                elsif ($password.length<=4)
                     puts "Enter password upto 5 digits with special character"
                     $password=gets.chomp
@@ -46,91 +47,99 @@ class TrainBooking
                          puts "Enter password does not match."
                     end
                else
-                    puts "Enter valid details.."
+                    puts "Enter valid details"
                end
                main_menu
      end
      def login
           puts "Enter your login details...."
           puts "Enter your username"
-          login_name=gets.chomp
+          $login_name=gets.chomp
           puts "Enter password"
-          login_pass=gets.chomp 
-          if (login_name==$username) && (login_pass==$password)
+          $login_pass=gets.chomp 
+          if ($login_name==$username) && ($login_pass==$password)
                puts "Sucessfully login!!!"
                puts
                puts "Please choose below options"
                puts "---------------------"
                while true
                     puts "1.Book ticket"
-                    puts "2.Cancel ticket"
-                    puts "3.Show ticket"
+                    puts "2.Show ticket"
+                    puts "3.Cancel ticket"
                     puts "4.main menu"
                     puts "5.Exit"
-                    choice=gets.chomp.to_i
-                    case(choice)
+                    select_choice=gets.chomp.to_i
+                    case(select_choice)
                     when 1
-                         file=File.open("traindetails.txt","r")
-                         puts
-                         puts file.read() 
-                         file.close()
-                         puts "All available trains.."
-                         puts "Enter your source point"
-                         source=gets.chomp
-                         puts
-                         puts "Enter destination point"
-                         destination=gets.chomp
-                         puts
-                         puts "Enter day of Journey"
-                         day=gets.chomp.to_i
-                         puts "Enter month of Journey"
-                         month=gets.chomp.to_i
-                         puts
-                         puts "---------------------"
-                         puts "Enter passenger details.."
-                         puts "---------------------"
-                         puts "Enter number of tickets you want"
-                         ticket_number=gets.chomp.to_i
-                         puts 
-                         puts "Enter passenger name"
-                         passenger_name=gets.chomp
-                         puts 
-                         puts "Enter Username and password for ticket booking"
-                         log_password=gets.chomp
-                         if (log_password==login_pass)
-                              puts "Ticket booked sucessfully!!!!"
-                              puts "Enjoy your journey.."
-                         else
-                              puts "You have entered wrong username and password ..."
-                         end
-                         puts
                          ticket_booking
-
-                         exit
                     when 2
-                         puts 
+                         show_ticket
                     when 3
-                         puts 
+                         cancel_ticket
                     end
                end
           else 
-               puts "Enter valid details.."
+               puts "Invalid details.."
+          end
+     end
+     def ticket_booking
+          file=File.open("traindetails.txt","r")
+          puts
+          puts file.read() 
+          file.close()
+          puts "All available trains.."
+          puts "Enter your source point"
+          source=gets.chomp
+          puts
+          puts "Enter destination point"
+          destination=gets.chomp
+          puts
+          puts "Enter day of Journey"
+          day=gets.chomp.to_i
+          puts "Enter month of Journey"
+          month=gets.chomp.to_i
+          puts
+          puts "Enter passenger age"
+          passenger_age=gets.chomp.to_i
+          puts 
+          puts "Enter Username and password for ticket booking"
+          log_name=gets.chomp
+          log_password=gets.chomp
+          if (log_name==$login_name && log_password==$login_pass)
+               puts "Ticket booked sucessfully!!!!"
+               puts "Enjoy your journey.."
+          else
+               puts "You have entered wrong username and password ..."
+               exit
+          end
+          puts
+          ticketBooking = {"source" => source,"destination"=>destination,"day"=>day,"month"=>month,"Ticket_number"=>$ticket_number,"Passanger age"=>passenger_age,"Mobile Number"=>$number}
+          File.open("TicketBooking.txt" , "w") do |file|
+          file.write(ticketBooking)
+          end
+     end
+     def cancel_ticket
+          puts "Enter ticket number you want to cancel"
+          ticket_num=gets.chom.to_i
+          if ticket_num==$ticket_number
+               file = File.open("traindetails.txt" , "r+")
+               File.delete(file)
+               puts "Ticket Canceled Succesfully...!"
+          else
+               puts "Please try again!!!"
+               main_menu
           end
      end
      def wrong_input
-          if ($choice>3 || $choice<1)
-               puts "You have entered wrong choice.."
+          if ($select_choice>3 || $select_choice<1)
+               puts "You have entered wrong select_choice.."
                puts "Do you want to continue (Y/N):"
-               $choice=gets.chomp
-          
-          while ($choice=="y" || $choice=="Y")
-               puts "Enter right choice.."
-               $choice=gets.chomp.to_i
-          
-               main_menu
-               
+               $select_choice=gets.chomp
+               while ($select_choice=="y" || $select_choice=="Y")
+                    puts "Enter right select_choice.."
+                    $select_choice=gets.chomp.to_i
+                    main_menu
                end
-          
           end
      end
 end
